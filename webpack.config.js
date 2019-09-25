d26a7df2e8ba9f866d9f1e85a797175ff46541bd
Pliavi <pliavi.rpgm@gmail.com>
2019-09-25 11:16:33 -0300

    Installing and configuring Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      lib: path.resolve(__dirname, "./lib")
+    }
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
